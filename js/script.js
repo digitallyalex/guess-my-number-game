ab073bb2e3f4a5a81bb4a6f44cc4c9b11ee20d09
Alexandra Asanache <ale.asanache@gmail.com>
2023-12-01 14:47:40 +0000

    Set secret random number

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+//selecting a secret random number between 1 and 20 for each game:
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
